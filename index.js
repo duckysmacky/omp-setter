780122aa5ed5a2dd0b1dc2038d70bb0bfa4e50b9
Ducky <kriperoka@gmail.com>
2024-05-18 16:14:24 +0300

    working theme set

--- index.js
+++ index.js
@@ -1,14 +1,47 @@
+#!/usr/bin/env node
 const { exec } = require("child_process");
+const fs = require("fs");
 
-function getProfile(_callback) {
-    exec("$PROFILE", {"shell": "powershell.exe"}, (error, stdout, stderr) => {
-        if (error) {
-            console.log(`An error has occured getting $PROFILE path: ${error.name} (code : ${error.code})`);
-            process.exit(1);
-        }
-    
-        _callback(stdout.trim());
+async function getProfile() {
+    return new Promise((resolve, reject) => {
+        exec("$PROFILE", {shell: "powershell.exe"}, (error, stdout, stderr) => {
+            if (error) {
+                console.error(`An error has occured getting $PROFILE path: ${error.name} (code : ${error.code})`);
+                process.exit(1);
+            }
+            
+            resolve(stdout.trim());
+        });
     });
 }
 
-getProfile((profile) => console.log(profile));
+async function applyProfile() {
+    return new Promise((resolve, reject) => {
+        exec("& $PROFILE", {shell: "powershell.exe"}, (error, stdout, stderr) => {
+            if (stderr) reject(stderr);
+            resolve("Applied profile!");
+        });
+    });
+}
+
+async function setTheme(theme) {
+    var path = await getProfile();
+    var profile = fs.readFileSync(path, "utf8");
+
+    profile = profile.replace(/themes\\.*.omp/, `themes\\${theme}.omp`);
+
+    fs.writeFileSync(path, profile);
+
+    // console.log(await applyProfile());
+    // console.log(profile);
+}
+
+var theme = process.argv[2];
+
+if (!theme) {
+    console.error("Error: theme not specified!");
+    console.log("Usage: omptheme <theme name>");
+    process.exit(1);
+}
+
+setTheme(theme);
